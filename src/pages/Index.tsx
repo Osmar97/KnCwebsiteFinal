948f8ad91b68cc9d47591f03e1f07e350f6780b9
Osmar <92826218+Osmar97@users.noreply.github.com>
2025-05-31 11:37:03 +0100

    remove company description section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,22 +12,6 @@ const Index = () => {
       <Navigation />
       <Hero />
       
-      {/* Company Introduction Section */}
-      <section className="py-20 bg-black">
-        <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <div className="mb-8">
-            <img 
-              src="/lovable-uploads/bf8148de-409a-4bd5-88c1-11dca2d5bdab.png" 
-              alt="Kings 'n Company Logo" 
-              className="mx-auto max-w-md w-full h-auto"
-            />
-          </div>
-          <p className="text-gray-300 text-lg md:text-xl font-light leading-relaxed italic">
-            A real estate investment consultancy firm that seamlessly blends the strengths of regional market expertise 
-            with an expansive network of international investors and partners.
-          </p>
-        </div>
-      </section>
       
       {/* Sections Carousel */}
       <div className="bg-black">
