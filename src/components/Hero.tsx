ddfa352100e790a10bf2edd27caaa9baeb8fcfc9
Osmar <92826218+Osmar97@users.noreply.github.com>
2025-08-12 19:20:00 +0100

    new

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,7 @@
-
 import ScrollExpandMedia from "@/components/ScrollExpandMedia";
 
 const HeroContent = () => {
-  return (
-    <div className="max-w-6xl mx-auto text-center">
-      <div className="w-24 h-0.5 bg-gradient-to-r from-transparent via-gold to-transparent mx-auto mb-8"></div>
-      
-      <p className="text-gray-300 text-lg md:text-xl mb-3 max-w-3xl mx-auto leading-relaxed font-bold">
-        Connecting Visionary Investors to Remarkable Properties
-      </p>
-      
-      <p className="text-white text-xl mb-12 max-w-3xl mx-auto leading-relaxed md:text-base font-extralight py-[2px]">
-        Welcome to Kings 'n Company
-      </p>
-    </div>
-  );
+  return null; // Nothing rendered
 };
 
 export const Hero = () => {
